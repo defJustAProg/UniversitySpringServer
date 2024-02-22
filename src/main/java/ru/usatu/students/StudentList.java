84a765c620ca34bf18af4aaa2512aed49d085781
Roman <roman.worker.email@mail.ru>
2024-02-23 00:15:43 +0500

    First commit(template)

--- src/main/java/ru/usatu/students/StudentList.java
+++ src/main/java/ru/usatu/students/StudentList.java
@@ -1,19 +1,31 @@
+package ru.usatu.students;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import ru.usatu.students.Student;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ru.usatu.students;
-
-import java.util.List;
 
 /**
  *
  * @author Admin
  */
-class StudentList {
-
-    List<Student> getStudents() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+@XmlRootElement(name="students")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class StudentList {
+    @XmlElement(name="student")
+    private List<Student> students;
+    
+    public List<Student> getStudents(){
+        return students;
     }
     
+    public void setStudents(List<Student> students){
+        this.students=students;
+    }
 }
